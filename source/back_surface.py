0947bf2e2f4f123ed938700de67ebd9031c6cccb
Chinh Tran <chinhpfiev@gmail.com>
2020-03-27 17:18:39 -0400

    reverse points

--- source/back_surface.py
+++ source/back_surface.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math
+import points_distance as ptd
 
-def groupYAxis(front_points,delta):
+def groupYAxis(front_points,delta,radius):
     #sap xep theo chieu tang dan cua toa do y
     temp = sortColumn(front_points,1)
     #tao mang gop nhom
@@ -14,6 +15,7 @@ def groupYAxis(front_points,delta):
         group[num].append(list(temp[i]))
     for i in range(group_count):
         group[i] = sortColumn(group[i],0)
+        getArea(group[i],radius)
     return group
 
 def sortColumn(list_or_array,col_num):
@@ -21,4 +23,39 @@ def sortColumn(list_or_array,col_num):
     #sap xep theo chieu tang dan cua toa do y
     return a[a[:,col_num].argsort()]
 
-#def getArea(list_or_array):
+def getArea(list_or_array,radius):
+    result = []
+    rule = True
+    count = len(list_or_array)
+    for i in range(count):
+        if rule:
+            result.append([])
+            result[-1].append(list_or_array[i])
+            if count != (i+1):
+                if ptd.checkNeighbor(list_or_array[i],list_or_array[i+1],radius):
+                    rule = False
+        else:
+            result[-1].append(list_or_array[i])
+            if count != (i+1):
+                if not ptd.checkNeighbor(list_or_array[i],list_or_array[i+1],radius):
+                    rule = True
+    print('The array are divided into '+str(len(result))+' area(s)')
+    return np.asarray(result)
+
+def reverseCurve(points):
+    temp = points.copy()
+    center_x = (temp[0][0]+temp[-1][0])/2
+    center_z = (temp[0][2]+temp[-1][2])/2
+    print(center_x,center_z)
+    for i in temp:
+        i[0] = 2*center_x - i[0]
+        i[2] = 2*center_z - i[2]
+    return np.asarray(temp)
+
+test = [[-2,1,3],
+        [-1,0,1],
+        [1,6,1],
+        [2,5,2]]
+
+print(reverseCurve(test))
+
--- source/points_distance.py
+++ source/points_distance.py
@@ -3,3 +3,10 @@ import math
 
 def distance(x1,y1,z1,x2,y2,z2):
     return math.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+
+def checkNeighbor(numy_array_1,numy_array_2,radius):
+    temp = distance(numy_array_1[0],numy_array_1[1],numy_array_1[2],numy_array_2[0],numy_array_2[1],numy_array_2[2])
+    if temp<=radius:
+        return True
+    else:
+        return False
