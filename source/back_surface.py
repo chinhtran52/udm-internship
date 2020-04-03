e9a8cbdde765eeff9f2860e18139b1ab45f7be45
Chinh Tran <chinhpfiev@gmail.com>
2020-04-03 10:53:29 -0400

    3/4/2020

--- source/back_surface.py
+++ source/back_surface.py
@@ -29,8 +29,7 @@ def getArea(list_or_array,radius):
     count = len(list_or_array)
     for i in range(count):
         if rule:
-            result.append([])
-            result[-1].append(list_or_array[i])
+            result.append([list_or_array[i]])
             if count != (i+1):
                 if ptd.checkNeighbor(list_or_array[i],list_or_array[i+1],radius):
                     rule = False
@@ -41,6 +40,18 @@ def getArea(list_or_array,radius):
                     rule = True
     #print('The array are divided into '+str(len(result))+' area(s)')
     return np.asarray(result)
+
+def checkVertex(list,point_index):
+    if list[point_index][2]>list[point_index-1][2] and list[point_index][2]>list[point_index+1][2]:
+        return False
+    else:
+        return True
+
+def getProjection(points,shift=0):
+    temp = points.copy()
+    for i in range(len(temp)):
+        temp[i][2] = temp[0][2]+((temp[-1][2]-temp[0][2])*(temp[i][0]-temp[0][0])/(temp[-1][0]-temp[0][0])) + shift
+    return np.asarray(temp)
 
 def reverseCurve(points,shift=0):
     temp = points.copy()
@@ -65,5 +76,7 @@ def generateBack(front_points,delta,radius,shift=0):
         for j in i:
             temp = np.asarray(j)
             results.append(reverseCurve(temp,shift))
+            #results.append(getProjection(temp,shift))
     results = reshape(results)
     return np.asarray(results)
+
