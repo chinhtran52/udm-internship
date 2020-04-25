6aece0e16687b1728fde5d8df5c58c3e6d41d73b
Chinh Tran <chinhpfiev@gmail.com>
2020-04-25 14:22:24 -0400

    avg curvature by color range

--- source/Colors/gradient_color.py
+++ source/Colors/gradient_color.py
@@ -1,16 +1,16 @@
 import numpy as np
 from colour import Color
 
-
-def getColor(value,range_color=32,highest=10,lowest=-10):
+range_color = 32
+red = Color("blue")
+colors = np.array(list(red.range_to(Color("red"),range_color)))
+def getColor(value,highest=2,lowest=-2):
     if lowest<value<highest:
         temp = value
     elif value<=lowest:
         temp = lowest
     else:
         temp = highest
-    red = Color("blue")
-    colors = np.array(list(red.range_to(Color("red"),range_color)))
     ran = highest - lowest
     det = temp - lowest
     cur = int((det*(range_color-1))//ran)
---
+++ source/Mesh/results/8 frames/readme.md
@@ -0,0 +1,18 @@
+## letter for file name:
+### Type of walking
+    1. Normal   : n
+    2. Fixed    : f
+    3. Half     : h
+### Type of curvature:
+    1. Gaussian : g
+    2. Mean     : m
+    3. Absolute : a
+    4. RMSquare : r
+### Mesh format:
+    PLY : Polygon File format
+### Convention:
+```
+code = <type of walking> + <type of curvature> + <pose number> + <.ply>
+```
+### Example:
+- ng1.ply = normal walking Gaussian curvature pose 1 mesh format
