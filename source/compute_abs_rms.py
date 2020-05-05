267ffea4febaa97095b4cc768f6922a31f40abf1
Chinh Tran <chinhpfiev@gmail.com>
2020-05-04 20:46:23 -0400

    4/5/2020

--- source/compute_abs_rms.py
+++ source/compute_abs_rms.py
@@ -6,17 +6,16 @@ import math
 
 def getPrincipalCurve(frames,gauss_dic_in,mean_dic_in,abs_dic_out,rms_dic_out):
     for i in range(1,frames+1):
-        file_name = 'pose_'+str(i)+'.ply'
-        plydata_mean = PlyData.read(mean_dic_in+file_name)
-        plydata_gauss = PlyData.read(gauss_dic_in+file_name)
+        plydata_mean = PlyData.read(mean_dic_in+'jean_nm'+str(i)+'.ply')
+        plydata_gauss = PlyData.read(gauss_dic_in+'jean_ng'+str(i)+'.ply')
         mean_data = np.asarray(plydata_mean['vertex'][:]['quality'])
         gauss_data = np.asarray(plydata_gauss['vertex'][:]['quality'])
         count = plydata_mean['vertex'].count
-        for i in range(count):
-            s = 2*mean_data[i]
-            p = gauss_data[i]
+        for j in range(count):
+            s = 2*mean_data[j]
+            p = gauss_data[j]
             k = qdr.quadratic_revert_viete(s,p)
-            plydata_mean['vertex'][i]['quality'] = abs(k[0]) + abs(k[1])
-            plydata_gauss['vertex'][i]['quality'] = math.sqrt((k[0]*k[0]+k[1]*k[1])/2)
-        plydata_mean.write(abs_dic_out+file_name)
-        plydata_gauss.write(rms_dic_out+file_name)
+            plydata_mean['vertex'][j]['quality'] = abs(k[0]) + abs(k[1])
+            plydata_gauss['vertex'][j]['quality'] = math.sqrt((k[0]*k[0]+k[1]*k[1])/2)
+        plydata_mean.write(abs_dic_out+'jean_na'+str(i)+'.ply')
+        plydata_gauss.write(rms_dic_out+'jean_nr'+str(i)+'.ply')
